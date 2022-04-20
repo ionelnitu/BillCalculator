cf72b6c2f958c4cf57445ade4834e3c6a826e3e7
ionelnitu <ionelnitu73@gmail.com>
2022-04-20 18:20:38 +0300

    Update Controller.java

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -15,64 +15,70 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class Controller {
-    static MySql mySql=new MySql();
+    static MySql mySql = new MySql();
 
 
     @FXML
-    TextField emailTF,usernameTF,passwordTF,userNameMain;
+    TextField emailTF, usernameTF, passwordTF, userNameMain;
     @FXML
     PasswordField passwordMain;
 
     @FXML
     private void createUserFrame(ActionEvent actionEvent) throws IOException {
-        Parent parent= FXMLLoader.load(getClass().getClassLoader().getResource("resources/createFrame.fxml"));
-        Scene scene= new Scene(parent);
-        Stage stage=(Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("resources/createFrame.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
 
     @FXML
-    private  void backToHome(ActionEvent actionEvent) throws IOException {
-        Parent parent=FXMLLoader.load(getClass().getClassLoader().getResource("resources/mainFrame.fxml"));
-        Scene scene=new Scene(parent);
-        Stage stage=(Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    private void backToHome(ActionEvent actionEvent) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("resources/mainFrame.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
 
     @FXML
-private  void logIn(ActionEvent actionEvent) throws SQLException, IOException {
-        Parent parent=FXMLLoader.load(getClass().getClassLoader().getResource("resources/loginFrame.fxml"));
-        Scene scene=new Scene(parent);
-        Stage stage=(Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        if(mySql.verify(userNameMain.getText(),passwordMain.getText())){
-            Alert alert=new Alert(Alert.AlertType.INFORMATION);
+    private void logIn(ActionEvent actionEvent) throws SQLException, IOException {
+        Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("resources/loginFrame.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        if (mySql.verify(userNameMain.getText(), passwordMain.getText())) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setContentText("LogIn Successfully!");
             alert.setHeaderText(null);
             alert.showAndWait();
             stage.setScene(scene);
             stage.show();
 
-        }else {
-            Alert alert=new Alert(Alert.AlertType.ERROR);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setContentText("Please verify you username and password!");
             alert.setHeaderText(null);
             alert.showAndWait();
         }
 
-}
+    }
 
-@FXML
-    private void create() {
-        try{
-            mySql.insert(usernameTF.getText(),passwordTF.getText(),emailTF.getText());
-        }catch (SQLException e){
-            Alert alert=new Alert(Alert.AlertType.ERROR);
+    @FXML
+    private void create(ActionEvent actionEvent) throws IOException {
+        try {
+            mySql.insert(usernameTF.getText(), passwordTF.getText(), emailTF.getText());
+            Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("resources/mainFrame.fxml"));
+            Scene scene = new Scene(parent);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (SQLException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText(null);
             alert.setContentText("Please verify your data!");
             alert.showAndWait();
         }
 
-}
+    }
 }
